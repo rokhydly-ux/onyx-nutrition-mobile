385b24c6e9feb4b254300b9438bbf672348b2c36
rokhydly-ux <rokhydly@gmail.com>
2026-07-13 15:52:19 +0000

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }]
     ],
   };
 };
